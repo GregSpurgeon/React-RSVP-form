f02cf38033b19d5aa466a6b36637a84c6b877fb8
Greg Spurgeon <spurgeon.greg11@gmail.com>
2020-07-08 18:20:37 -0400

    functioning mongoose db connection and data storage

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert')
-// import Response from './attendees'
+const mongoose= require("mongoose") ;
 
 const app =express()
 const port =3000
@@ -12,34 +12,54 @@ app.use(express.urlencoded({extended:false}))
 app.set('views', './views')
 app.set('view engine', 'pug')
 
+//Connection URL
+const url = 'mongodb://localhost/rsvp';
+
+
+//use connect method to connect to the server
+mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true},function(){
+  console.log('Connected successfully to server');
+})
+
+const GuestSchema = mongoose.Schema(
+  {
+    name:String,
+    email:String,
+    attending:Boolean,
+    guests:Number,
+  });
+
+const Response = mongoose.model("Guests", GuestSchema);
+
+app.post("/reply", async (req,res) =>{
+    // console.log(req.body)
+    const response = new Response({
+      name:req.body.name,
+      email:req.body.email,
+      attending:req.body.attending,
+      guests:req.body.guests
+    });
+    // console.log(response)
+    try{
+      await response.save()
+      res.render('reply',{title:"Thank you for your response!"})
+    }catch(err){
+      res.send("Error")
+    }
+  })
+  
 app.get("/", (req,res) =>{
     res.render('index',{title:"RSVP"})
 })
 
-app.post("/reply", (req,res) =>{
-  res.render('reply',{title:"Thank you for your response!"})
-})
-
 app.get("/guests", (req,res) =>{
-  res.render('guests',{title:"Guest List"})
+  Response.find({attending:true},function(err,attending){
+    Response.find({attending:false},function(err,notAttending){
+      res.render('guests',{title:"Guest List",attending,notAttending})
+    })
+    // console.log({people:response})
+  })
 })
-
-
-//Connection URL
-const url = 'mongodb://localhost:27017';
-
-//Database Name
-const dbName= 'rsvp';
-
-//use connect method to connect to the server
-MongoClient.connect(url, function(err,client){
-  assert.equal(null, err);
-  console.log('Connected successfully to server');
-
-  const db= client.db(dbName);
-    client.close();
-})
-
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
